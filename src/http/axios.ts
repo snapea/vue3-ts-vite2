ef41e697dd6f4ecaeeafca7bd7e7b2d2a8ffeee1
daye99 <848403550@qq.com>
2021-08-04 09:51:16 +0800

    fix: axios can't request

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -2,7 +2,7 @@
  * @Author: wenzhenjin
  * @Date: 2021-04-23 15:16:08
  * @LastEditors: wenzhenjin
- * @LastEditTime: 2021-04-25 16:06:48
+ * @LastEditTime: 2021-08-04 09:50:19
  * @Description:
  */
 import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse } from 'axios'
@@ -26,17 +26,21 @@ class Axios {
         instance.interceptors.request.use(
             (config: AxiosRequestConfig) => {
                 // 请求之前的操作
+                return config
             },
             (error: any) => {
                 // 错误
+                return Promise.reject(error)
             }
         )
         instance.interceptors.response.use(
             (response: AxiosResponse) => {
                 // 响应成功
+                return response.data
             },
             (error: any) => {
                 // 响应拦截
+                return Promise.reject(error)
             }
         )
     }
